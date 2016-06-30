02f9cd1ce2eeb2b595a58305ca9855ac3a652f81
mfahmizamroni <mfahmizamroni@gmail.com>
2016-06-30 09:48:18 +0800

    baru lagi

--- application/views/mis/pages/dashboard.php
+++ application/views/mis/pages/dashboard.php
@@ -56,6 +56,120 @@ include('sidebar.php');
           </div>
         </div>  
       </div>
+            <div class="row">
+        <!-- /.col -->
+        <div class="col-md-4">
+          <!-- Widget: user widget style 1 -->
+          <div class="box box-widget widget-user-2">
+            <!-- Add the bg color to the header using any of the bg-* classes -->
+            <div class="widget-user-header bg-green">
+              <div class="widget-user-image">
+                <img class="img-circle" src="http://ecovata-montazh.ru/wp-content/uploads/2015/09/c82601c2d502767c58b78d6e2d01d49f.png" alt="User Avatar">
+              </div>
+              <h3 class="widget-user-username">Lati</h3>
+              <h5 class="widget-user-desc">Mine Operation</h5>
+            </div>
+            <div class="box-footer">
+              <div class="row">
+                <div class="col-sm-6 border-right">
+                  <div class="description-block">
+                    <h5 class="description-header">3,200</h5>
+                    <span class="description-text">KONTRAK</span>
+                  </div>
+                  <!-- /.description-block -->
+                </div>
+                <!-- /.col -->
+                <div class="col-sm-6 border-right">
+                  <div class="description-block">
+                    <h5 class="description-header">13,000</h5>
+                    <span class="description-text">MATERIAL</span>
+                  </div>
+                  <!-- /.description-block -->
+                </div>
+                <!-- /.col -->
+              </div>
+              <!-- /.row -->
+            </div>
+          </div>
+          <!-- /.widget-user -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-4">
+          <!-- Widget: user widget style 1 -->
+          <div class="box box-widget widget-user-2">
+            <!-- Add the bg color to the header using any of the bg-* classes -->
+            <div class="widget-user-header bg-green">
+              <div class="widget-user-image">
+                <img class="img-circle" src="http://ecovata-montazh.ru/wp-content/uploads/2015/09/c82601c2d502767c58b78d6e2d01d49f.png" alt="User Avatar">
+              </div>
+              <h3 class="widget-user-username">Binungan</h3>
+              <h5 class="widget-user-desc">Mine Operation</h5>
+            </div>
+            <div class="box-footer">
+              <div class="row">
+                <div class="col-sm-6 border-right">
+                  <div class="description-block">
+                    <h5 class="description-header">3,200</h5>
+                    <span class="description-text">KONTRAK</span>
+                  </div>
+                  <!-- /.description-block -->
+                </div>
+                <!-- /.col -->
+                <div class="col-sm-6 border-right">
+                  <div class="description-block">
+                    <h5 class="description-header">13,000</h5>
+                    <span class="description-text">MATERIAL</span>
+                  </div>
+                  <!-- /.description-block -->
+                </div>
+                <!-- /.col -->
+              </div>
+              <!-- /.row -->
+            </div>
+          </div>
+          <!-- /.widget-user -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-4">
+          <!-- Widget: user widget style 1 -->
+          <div class="box box-widget widget-user-2">
+            <!-- Add the bg color to the header using any of the bg-* classes -->
+            <div class="widget-user-header bg-green">
+              <div class="widget-user-image">
+                <img class="img-circle" src="http://ecovata-montazh.ru/wp-content/uploads/2015/09/c82601c2d502767c58b78d6e2d01d49f.png" alt="User Avatar">
+              </div>
+              <h3 class="widget-user-username">Sambarata</h3>
+              <h5 class="widget-user-desc">Mine Operation</h5>
+            </div>
+            <div class="box-footer">
+              <div class="row">
+                <div class="col-sm-6 border-right">
+                  <div class="description-block">
+                    <h5 class="description-header">3,200</h5>
+                    <span class="description-text">KONTRAK</span>
+                  </div>
+                  <!-- /.description-block -->
+                </div>
+                <!-- /.col -->
+                <div class="col-sm-6 border-right">
+                  <div class="description-block">
+                    <h5 class="description-header">13,000</h5>
+                    <span class="description-text">MATERIAL</span>
+                  </div>
+                  <!-- /.description-block -->
+                </div>
+                <!-- /.col -->
+              </div>
+              <!-- /.row -->
+            </div>
+          </div>
+          <!-- /.widget-user -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+
+
 
     </section>
     <!-- /.content -->
